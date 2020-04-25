e95cba82a883df9eb65f86d8c070ad2d9b66d049
Dheeraj Kesri <kesridheeraj@yahoo.com>
2020-04-25 20:16:02 +0530

    Add math functionality

---
+++ GithubDemo/Maths.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GithubDemo {
+    public class Maths {
+        public int Add(int a, int b) {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b) {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b) {
+            return a * b;
+        }
+
+        public int Divide(int a, int b) {
+            return a / b;
+        }
+    }
+}
---
+++ GithubDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GithubDemo {
+    class Program {
+        static void Main(string[] args) {
+            Maths math = new Maths();
+            int a = 10;
+            int b = 5;
+
+            var sum = math.Add(a, b);
+            var diff = math.Subtract(a, b);
+            var multiplication = math.Multiply(a, b);
+            var division = math.Divide(a, b);
+        }
+    }
+}
